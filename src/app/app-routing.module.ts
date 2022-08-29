53f99ecbbdb982e9e87a124c2e6541321c19cd27
diegorv21 <di.riveraq@duocuc.cl>
2022-08-28 21:27:07 -0400

    se agrega el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'ingreso',
+    loadChildren: () => import('./ingreso/ingreso.module').then( m => m.IngresoPageModule)
+  },
 ];
 
 @NgModule({
